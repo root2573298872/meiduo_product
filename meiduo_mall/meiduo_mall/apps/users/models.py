a3db43cda68be0967c4a4e374dddde494b0e529f
meiduo <2573298872@qq.com>
2021-05-02 21:09:04 +0800

    10_用户注册后端

--- meiduo_mall/meiduo_mall/apps/users/models.py
+++ meiduo_mall/meiduo_mall/apps/users/models.py
@@ -1,15 +1,20 @@
 from django.db import models
 from django.contrib.auth.models import AbstractUser
 
+
 # Create your models here.
 
 
 class User(AbstractUser):
-    """自定义用户模型类"""
+    """
+    自定义扩充模型类
+    """
     mobile = models.CharField(max_length=11, unique=True, verbose_name='手机号')
+    email_active = models.BooleanField(default=False, verbose_name='邮箱验证状态')
 
+    # 自定义表的名字
     class Meta:
-        db_table = 'tb_users'
+        db_table = 'tb_user'
         verbose_name = '用户'
         verbose_name_plural = verbose_name
 
--- meiduo_mall/meiduo_mall/static/js/register.js
+++ meiduo_mall/meiduo_mall/static/js/register.js
@@ -28,6 +28,22 @@ let vm = new Vue({
                 this.error_name = true
                 this.error_name_message = '请输入5-20个字符的用户名'
             }
+            if(this.error_name == false){
+                let url = '/usernames/'+ this.username + '/count/'
+                axios.get(url,{responseType: 'json'})
+                    .then(response=>{
+                        if(response.data.count==1){
+                            this.error_name_message = '用户名已存在';
+                            this.error_name = true;
+                        }
+                        else {
+                            this.error_name = false
+                        }
+                    })
+                .catch(error => {
+                    console.log(error.response);
+        })
+            }
         },
         // 校验密码
         check_password(){
@@ -55,6 +71,26 @@ let vm = new Vue({
                 this.error_mobile = true
                 this.error_mobile_message = '您输入的手机号格式不正确'
             }
+
+
+               if(this.error_mobile == false){
+                let url = '/mobiles/'+ this.mobile + '/count/'
+                axios.get(url,{responseType: 'json'})
+                    .then(response=>{
+                        if(response.data.count==1){
+                            this.error_mobile_message = '手机号已存在';
+                            this.error_mobile = true;
+                        }
+                        else {
+                            this.error_mobile = false
+                        }
+                    })
+                .catch(error => {
+                    console.log(error.response);
+        })
+            }
+
+
         },
         // 校验是否勾选协议
         check_allow(){
@@ -72,7 +108,7 @@ let vm = new Vue({
             this.check_mobile()
             this.check_allow()
             if(this.error_name == true || this.error_password == true || this.error_password2 == true || this.error_mobile == true || this.error_allow == true){
-                return window.event.returnValue  = false
+                window.event.returnValue = false;
             }
         },
 
